f7c5cfd01ffd9abfb70e83f9f13190f91a8d8850
NicoSelomin <vodounnounicodemeselomin@gmail.com>
2024-09-14 10:33:17 +0100

    updated

--- js/events.js
+++ js/events.js
@@ -1,15 +1,18 @@
 const maBoite = document.getElementById("maBoite");
 
 function ajouterAnimation(animationProps) {
-    gsap.fromTo(maBoite, animationProps.from, animationProps.to);
+    gsap.to(maBoite, {
+        duration: animationProps.duration,
+        ...animationProps.to
+    });
 }
 
 function passerSouri() {
     maBoite.innerHTML = `<p class="text-center text-white fw-bold fs-2">Tu viens de passer sur mon domaine</p>`;
     maBoite.style.backgroundColor = "purple";
     ajouterAnimation({
-        from: { scale: 0.5, opacity: 0 },
-        to: { scale: 1, opacity: 1, duration: 0.5 }
+        duration: 0.3,
+        to: { scale: 1.1, opacity: 1 }
     });
 }
 
@@ -17,8 +20,8 @@ function click() {
     maBoite.innerHTML = `<p class="text-center text-white fw-bold fs-2">Tu viens de cliquer</p>`;
     maBoite.style.backgroundColor = "green";
     ajouterAnimation({
-        from: { x: -100, opacity: 0 },
-        to: { x: 0, opacity: 1, duration: 0.5 }
+        duration: 0.3,
+        to: { x: 30, opacity: 1 }
     });
 }
 
@@ -26,8 +29,8 @@ function doubleClick() {
     maBoite.innerHTML = `<p class="text-center text-white fw-bold fs-2">Tu viens de double cliquer petit</p>`;
     maBoite.style.backgroundColor = "red";
     ajouterAnimation({
-        from: { rotation: -45, opacity: 0 },
-        to: { rotation: 0, opacity: 1, duration: 0.5 }
+        duration: 0.3,
+        to: { x: 0, rotation: 50, opacity: 1 }
     });
 }
 
@@ -35,17 +38,17 @@ function mouseout() {
     maBoite.innerHTML = `<p class="text-center text-white fw-bold fs-2">Tu viens d'enlever ta souris <br> Terrain neutre</p>`;
     maBoite.style.backgroundColor = "gray";
     ajouterAnimation({
-        from: { y: 50, opacity: 0 },
-        to: { y: 0, opacity: 1, duration: 0.5 }
+        duration: 0.3,
+        to: { x: 0, rotation:0, y: 0, opacity: 1 }
     });
 }
 
 function clickDroit() {
-    maBoite.innerHTML = `<p class="text-center text-white fw-bold fs-2">Tu as fais un clique droit</p>`;
+    maBoite.innerHTML = `<p class="text-center text-white fw-bold fs-2">Tu as fais un clic droit</p>`;
     maBoite.style.backgroundColor = "orange";
     ajouterAnimation({
-        from: { scale: 0.2, opacity: 0 },
-        to: { scale: 1, opacity: 1, duration: 0.5 }
+        duration: 0.3,
+        to: { scale: 1, opacity: 1 }
     });
 }
 
